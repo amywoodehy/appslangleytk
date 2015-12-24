1a3638a892c562020c1ff8cab480d1218bb9f644
amywoodehy <amywoodehy@outlook.com>
2015-12-24 22:24:46 +0600

    настройки для продакшена

--- appslangleytk/settings.py
+++ appslangleytk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!_-uhe))501qk(@4fb5ky2$^(qq-&2a-vaupw6biw(#*foz-_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
